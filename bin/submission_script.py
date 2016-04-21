209a9693560552bccfc9fbb1e847b1f0f67c1045
cmeb45 <cruss713@gmail.com>
2016-04-21 19:15:29 -0400

    Formatting tabs

--- bin/submission_script.py
+++ bin/submission_script.py
@@ -18,27 +18,27 @@ from sklearn.lda import LDA
 
 
 if __name__ == '__main__':
-	
-	# LOAD DATA 
-	# (Data transformations (add extra features,split into features/labels etc.)
+    
+    # LOAD DATA 
+    # (Data transformations (add extra features,split into features/labels etc.)
     # done a priori)
-	train = pd.read_csv('../input/data.csv')
-	test = pd.read_csv('../input/quiz.csv')
-	
-	# FEATURE EXTRACTION
-	pca = PCA(n_components = ...)
-	# Number of P.C. decided on which number does best on holdout
-	_ = pca.fit(train)
-	X_train = pca.transform(train)
-	X_test = pca.transform(test)
-	
-	# RUN MODEL WITH OPTIMIZED PARAMETERS
-	clf = ...
-	# Variable for specific classifier (decided on which one does best on holdout)
-	clf = clf.fit(X_train, y_train)
-	preds = clf.predict(X_test)
-	
-	# SUBMISSION FILE
-	idx = range(1,31710)
-	submission = pd.DataFrame({"id": idx, "prediction": preds})
+    train = pd.read_csv('../input/data.csv')
+    test = pd.read_csv('../input/quiz.csv')
+    
+    # FEATURE EXTRACTION
+    pca = PCA(n_components = ...)
+    # Number of P.C. decided on which number does best on holdout
+    _ = pca.fit(train)
+    X_train = pca.transform(train)
+    X_test = pca.transform(test)
+    
+    # RUN MODEL WITH OPTIMIZED PARAMETERS
+    clf = ...
+    # Variable for specific classifier (decided on which one does best on holdout)
+    clf = clf.fit(X_train, y_train)
+    preds = clf.predict(X_test)
+    
+    # SUBMISSION FILE
+    idx = range(1,31710)
+    submission = pd.DataFrame({"id": idx, "prediction": preds})
     submission.to_csv("some_ensemble_required_sub.csv", index=False)
