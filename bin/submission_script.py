c517be1b3a7fd3f1d1b7b8cb63ad6d32afe5035e
cmeb45 <cruss713@gmail.com>
2016-04-22 17:42:13 -0400

    Update submission_script.py

--- bin/submission_script.py
+++ bin/submission_script.py
@@ -15,21 +15,35 @@ from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.lda import LDA
+from sklearn.preprocessing import LabelEncoder
 
 
 if __name__ == '__main__':
     
     # LOAD DATA 
-    # (Data transformations (add extra features,split into features/labels etc.)
-    # done a priori)
     train = pd.read_csv('../input/data.csv')
     test = pd.read_csv('../input/quiz.csv')
+	
+	y = train.ix[:,-1].values
+	y_test = test.ix[:,-1].values
     
+	
+	X = train.drop(['16','7','56','57','35','32','31','29','2','11','9','0','5','14','label'], axis=1)
+	# Drop concatenated features
+	X_test = test.drop(['16','7','56','57','35','32','31','29','2','11','9','0','5','14'], axis=1)
+	# Drop concatenated features
+	
+	#X_factor_columns = list(set(X.columns.values.tolist()) - set(['59','60']))
+	X_dum = pd.get_dummies(X,columns=['8','17','18','20','23','25','26','58'])
+	X_dum_test = pd.get_dummies(X_test,columns=['8','17','18','20','23','25','26','58'])
+	
+	X_cols_to_use = list(X_dum.columns.difference(X_dum_test.columns))
+	X_test_cols_to_use = list(X_dum_test.columns.difference(X_dum.columns))
+	
     # FEATURE EXTRACTION
-    pca = PCA(n_components = ...)
+    pca = PCA(n_components = 0.8)
     # Number of P.C. decided on which number does best on holdout
-    _ = pca.fit(train)
-    X_train = pca.transform(train)
+    X_train = pca.fit_transform(train)
     X_test = pca.transform(test)
     
     # RUN MODEL WITH OPTIMIZED PARAMETERS
