1991848079bda83d5b55b4e8c68c78158489ba1b
cmeb45 <cruss713@gmail.com>
2016-05-01 20:43:19 -0400

    Revised script of models used

--- bin/project_models.py
+++ bin/project_models.py
@@ -17,15 +17,36 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.lda import LDA
 
 # LOAD DATA
-with open("featuresFactorized.npy/featuresFactorized.npy", "rb") as npy:
-    X = np.load(npy)
+train = pd.read_csv('../input/data.csv')
+test = pd.read_csv('../input/quiz.csv')
+
+n_test = np.shape(test)[0]
+
 # LOAD LABELS
-with open("labels.npy", "rb") as npy:
-    y = np.load(npy)
+y = train.ix[:,-1].values
+
+# DATA PREPROCESSING
+# Drop concatenated features
+X = train.drop(['16','7','56','57','35','32','31','29','2','11','9','0','5','14','label'], axis=1)
+X_test = test.drop(['16','7','56','57','35','32','31','29','2','11','9','0','5','14'], axis=1)
+
+# Binarize select factor variables
+X_dum = pd.get_dummies(X,columns=['8','17','18','20','23','25','26','58'])
+X_dum_test = pd.get_dummies(X_test,columns=['8','17','18','20','23','25','26','58'])
+
+# List of features in training set, but not in testing set
+X_cols_to_use = list(X_dum.columns.difference(X_dum_test.columns))
+
+# List of features in testing set, but not in training set
+X_test_cols_to_use = list(X_dum_test.columns.difference(X_dum.columns))
+
+# Drop features that are not in both training and testing sets
+X_dum_2 = X_dum.drop(X_cols_to_use,axis=1)
+X_dum_test_2 = X_dum_test.drop(X_test_cols_to_use,axis=1)
 
 # SEPARATE DATA INTO TRAINING & HOLDOUT DATA
 X_train, X_hold, y_train, y_hold = train_test_split(
-     X, y, test_size=0.3, random_state=42)
+     X_dum_2, y, test_size=0.3, random_state=42)
 y_train = y_train.astype(int)
 y_hold = y_hold.astype(int)
 
@@ -33,151 +54,37 @@ y_hold = y_hold.astype(int)
 cv_call = StratifiedKFold(y_train,n_folds=10)
 n_iter_search = 30
 
-##### MODEL 1: LINEAR REGRESSION #####
+##### MODEL 1: RANDOM FOREST #####
+
 
 # RUN RANDOMIZED SEARCH FOR HYPERPARAMETER OPTIMIZATION
-clf = Pipeline([('reduce_dim', PCA()),
-                ('model', LinearRegression())
- ])
+clf = RandomForestClassifier(random_state=42,n_jobs=-1)
 
-param_dist = {"reduce_dim__n_components": randint(2, 50)}
-# Random search for number of principal components
+param_dist = {n_estimators": randint(5, 200)}
 
 random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
                                    n_iter=n_iter_search,cv=cv_call,
-								   scoring='mean_squared_error')
+                                   scoring='accuracy')
 
 random_search = random_search.fit(X_train, y_train)
 
 # RETRIEVE OPTIMAL HYPERPARAMETER VALUES FROM RANDOM SEARCH
 best_parameters, score, _ = max(random_search.grid_scores_, key=lambda x: x[1])
-pca = PCA(n_components = best_parameters["reduce_dim__n_components"])
-_ = pca.fit(X_train)
-X_train_PCA = pca.fit_transform(X_train)
-clf = LinearRegression()
-
-# RUN MODEL WITH OPTIMIZED PARAMETERS
-clf = clf.fit(X_train_PCA, y_train)
-
-
-# MAKE PREDICTIONS ON HOLDOUT DATA
-X_hold_PCA = pca.transform(X_hold)
-y_hold_predict = clf.predict(X_hold_PCA)
-
-
-# The mean square error
-print("Residual sum of squares: %.2f"
-      % np.mean((y_hold_predict - y_hold) ** 2))
-
-
-##### MODEL 2: SUPPORT VECTOR MACHINE #####
-
-# RUN RANDOMIZED SEARCH FOR HYPERPARAMETER OPTIMIZATION
-
-clf = Pipeline([('reduce_dim', PCA()),
-                ('model', SGDClassifier(n_iter=5, random_state=42,n_jobs=-1,
-				class_weight="balanced"))
- ])
-
-param_dist = {"reduce_dim__n_components": randint(2, 50),
-              "model__alpha": expon(scale=.1),
-              "model__loss": ["hinge","log","modified_huber"],
-	      "model__penalty": ["l2","elasticnet"]}
-
-random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
-                                   n_iter=n_iter_search,cv=cv_call,
-								   scoring='f1')
-
-random_search = random_search.fit(X_train, y_train)
-
-# RETRIEVE OPTIMAL HYPERPARAMETER VALUES FROM RANDOM SEARCH
-best_parameters, score, _ = max(random_search.grid_scores_, key=lambda x: x[1])
-pca = PCA(n_components = best_parameters["reduce_dim__n_components"])
-_ = pca.fit(X_train)
-X_train_PCA = pca.fit_transform(X_train)
-clf = SGDClassifier(n_iter=5, random_state=42,n_jobs=-1,
-                    alpha=best_parameters["model__alpha"],
-					loss=best_parameters["model__loss"],
-					penalty=best_parameters["model__penalty"],
-					class_weight="balanced")
-
-# RUN MODEL WITH OPTIMIZED PARAMETERS
-clf = clf.fit(X_train_PCA, y_train)
-
-
-# MAKE PREDICTIONS ON HOLDOUT DATA
-X_hold_PCA = pca.transform(X_hold)
-y_hold_predict = clf.predict(X_hold_PCA)
-
-##### MODEL 3: RANDOM FOREST #####
-
-
-# RUN RANDOMIZED SEARCH FOR HYPERPARAMETER OPTIMIZATION
-clf = Pipeline([('reduce_dim', PCA()),
-                ('model', RandomForestClassifier(random_state=42,n_jobs=-1,
-				class_weight="balanced"))
- ])
-
-param_dist = {"reduce_dim__n_components": randint(2, 50),
-              "model__n_estimators": randint(5, 200)}
-
-random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
-                                   n_iter=n_iter_search,cv=cv_call,
-								   scoring='accuracy')
-
-random_search = random_search.fit(X_train, y_train)
-
-# RETRIEVE OPTIMAL HYPERPARAMETER VALUES FROM RANDOM SEARCH
-best_parameters, score, _ = max(random_search.grid_scores_, key=lambda x: x[1])
-pca = PCA(n_components = best_parameters["reduce_dim__n_components"])
-_ = pca.fit(X_train)
-X_train_PCA = pca.fit_transform(X_train)
 clf = RandomForestClassifier(random_state=42,n_jobs=-1,
-				class_weight="balanced",
-				n_components=best_parameters["model__n_estimators"])
+				n_components=best_parameters["n_estimators"])
 
 # RUN MODEL WITH OPTIMIZED PARAMETERS
-clf = clf.fit(X_train_PCA, y_train)
-
-
-# MAKE PREDICTIONS ON HOLDOUT DATA
-X_hold_PCA = pca.transform(X_hold)
-y_hold_predict = clf.predict(X_hold_PCA)
-
-
-##### MODEL 4: ADABOOST #####
-clf = Pipeline([('reduce_dim', PCA()),
-                ('model', AdaBoostClassifier(random_state=42))
- ])
-
-param_dist = {"reduce_dim__n_components": randint(2, 50),
-              "model__n_estimators": randint(5, 200),
-			  "model__learning_rate": [expon(scale=.1),1],
-			  "model__base_estimator": [DecisionTreeClassifier(),
-			                            SGDClassifier(random_state=42,
-										n_jobs=-1),
-										LDA()]}
-
-random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
-                                   n_iter=n_iter_search,cv=cv_call,
-								   scoring='accuracy')
-
-random_search = random_search.fit(X_train, y_train)
-
-# RETRIEVE OPTIMAL HYPERPARAMETER VALUES FROM RANDOM SEARCH
-best_parameters, score, _ = max(random_search.grid_scores_, key=lambda x: x[1])
-pca = PCA(n_components = best_parameters["reduce_dim__n_components"])
-_ = pca.fit(X_train)
-X_train_PCA = pca.fit_transform(X_train)
-clf =  AdaBoostClassifier(random_state=42,
-                         n_estimators=best_parameters["model__n_estimators"],
-						 learning_rate=best_parameters["model__learning_rate"],
-						 base_estimator=best_parameters["model__base_estimator"])
-
-# RUN MODEL WITH OPTIMIZED PARAMETERS
-clf = clf.fit(X_train_PCA, y_train)
-
+clf = clf.fit(X_train, y_train)
 
 # MAKE PREDICTIONS ON HOLDOUT DATA
-X_hold_PCA = pca.transform(X_hold)
-y_hold_predict = clf.predict(X_hold_PCA)
+preds_hold = clf.predict(X_hold)
+print "Holdout data overall accuracy: ", np.mean(preds_hold == y_hold)
+
+# MAKE PREDICTIONS ON TEST DATA
+preds = clf.predict(X_dum_test_2)
+    
+# SUBMISSION FILE
+idx = range(1,n_test)
+submission = pd.DataFrame({"id": idx, "prediction": preds})
+submission.to_csv("some_ensemble_required_sub.csv", index=False)
+
